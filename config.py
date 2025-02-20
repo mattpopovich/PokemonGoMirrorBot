5264a61d26eb78299d66d4ba45f600e0c8f83545
Matt Popovich <popovich.matt@gmail.com>
2025-02-20 11:43:07 -0700

    Adding coordinates

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ systems = {
     "iPhone15ProMaxWidescreen": {               # Widescreen monitor
         "start_trade_coordinates": [3377, 1285],
         "first_pokemon_coordinates": [3161, 953],
-        "between_first_second_pokemon": [3213, 947],
+        "between_first_second_pokemon": [3213, 947],    # Whitespace
         "next_button_coordinates": [3268, 1321],
         "confirm_button_coordinates": [3141, 1086],
-        "pokemon_details_left_health_white": [3146, 1021],
+        "pokemon_details_left_health_white": [3146, 1021],  # Whitespace left of the health bar
         "x_button_coordinates": [3268, 1389],
         "start_drag_next_poke": [3396, 1132],   # Right side of screen
         "end_drag_next_poke": [3147, 1132],     # Left side of screen
@@ -25,11 +25,13 @@ systems = {
     "iPhone11ProMBAirLarger": {                 # iPhone Mirroring -> View -> Larger
         "start_trade_coordinates": [1608, 871],
         "first_pokemon_coordinates": [1366, 489],
+        "between_first_second_pokemon": [1420, 488],
         "next_button_coordinates": [1487, 917],
         "confirm_button_coordinates": [1347, 650],
+        "pokemon_details_left_health_white": [1350, 579],
         "x_button_coordinates": [1486, 995],
-        "start_drag_next_poke": [1633, 710],    # Right side of screen
-        "end_drag_next_poke": [1340, 710],      # Left side of screen
+        "start_drag_next_poke": [1633, 710],
+        "end_drag_next_poke": [1340, 710],
         "modify_favorite": [1636,294],
         "change_delay": True
     },

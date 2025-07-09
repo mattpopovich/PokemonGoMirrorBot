46846285952bd8f61079480ac591d703f4665b87
Matt Popovich <popovich.matt@gmail.com>
2025-07-09 15:28:29 -0600

    Use utils.py in trader.py

--- trader.py
+++ trader.py
@@ -11,45 +11,7 @@ import config
 from cliclick import Cliclick
 import datetime
 
-
-def randomize_location(location, pixel_randomness):
-    """
-    Randomizes the location coordinates within a specified pixel_randomness range.
-
-    Args:
-    - location (list): A list in the form [x, y].
-    - pixel_randomness (int): Maximum random offset for both x and y.
-
-    Returns:
-    - list: New randomized location as [new_x, new_y].
-    """
-    location_x, location_y = location
-    random_offset_x = random.randint(-pixel_randomness, pixel_randomness)
-    random_offset_y = random.randint(-pixel_randomness, pixel_randomness)
-
-    new_x = location_x + random_offset_x
-    new_y = location_y + random_offset_y
-
-    return [new_x, new_y]
-
-def random_sleep(base_sleep_time_s, randomness_s):
-    """
-    Sleeps for a random duration based on base sleep time and randomness_s range.
-
-    Args:
-    - base_sleep_time_s (float): Base sleep time in seconds.
-    - randomness_s (float): Maximum random offset in seconds.
-
-    Returns:
-    - float: The actual sleep time.
-    """
-    offset = random.uniform(-randomness_s, randomness_s)
-    actual_sleep_time = max(0, base_sleep_time_s + offset)
-
-    print(f"  Sleeping for {actual_sleep_time:.2f}s")
-    time.sleep(actual_sleep_time)
-
-    return actual_sleep_time
+import functions.utils as utils
 
 # Access the coordinates from the active system
 start_trade_coordinates = config.SETTINGS['start_trade_coordinates']
@@ -79,7 +41,7 @@ same_pseudo_rng = random.Random(common_seed)
 cliclick = Cliclick()
 
 # Make screen mirroring the "active" window
-cliclick.click(randomize_location(start_trade_coordinates, pixel_randomness))
+cliclick.click(utils.randomize_location(start_trade_coordinates, pixel_randomness))
 
 num_trades = 35
 for i in range(num_trades):
@@ -92,24 +54,24 @@ for i in range(num_trades):
     initial_remaining_sleep = remaining_sleep
 
     print(f"Clicking on start trade")
-    cliclick.click(randomize_location(start_trade_coordinates, pixel_randomness))
-    remaining_sleep -= random_sleep(7.0, 1.5)
+    cliclick.click(utils.randomize_location(start_trade_coordinates, pixel_randomness))
+    remaining_sleep -= utils.random_sleep(7.0, 1.5)
 
     print("Clicking on first pokemon available to trade")
-    cliclick.click(randomize_location(first_pokemon_coordinates, pixel_randomness))
-    remaining_sleep -= random_sleep(5.0, 1.5)
+    cliclick.click(utils.randomize_location(first_pokemon_coordinates, pixel_randomness))
+    remaining_sleep -= utils.random_sleep(5.0, 1.5)
 
     print("Clicking on next")
-    cliclick.click(randomize_location(next_button_coordinates, pixel_randomness))
-    remaining_sleep -= random_sleep(5.0, 1.5)
+    cliclick.click(utils.randomize_location(next_button_coordinates, pixel_randomness))
+    remaining_sleep -= utils.random_sleep(5.0, 1.5)
 
     print("Clicking on confirm")
-    cliclick.click(randomize_location(confirm_button_coordinates, pixel_randomness))
-    remaining_sleep -= random_sleep(20.0, 1.5)
+    cliclick.click(utils.randomize_location(confirm_button_coordinates, pixel_randomness))
+    remaining_sleep -= utils.random_sleep(20.0, 1.5)
 
     print("Clicking on X")
-    cliclick.click(randomize_location(x_button_coordinates, pixel_randomness))
-    remaining_sleep -= random_sleep(5.0, 1.5)
+    cliclick.click(utils.randomize_location(x_button_coordinates, pixel_randomness))
+    remaining_sleep -= utils.random_sleep(5.0, 1.5)
 
     print(f"That trade took {(initial_remaining_sleep - remaining_sleep):.2f}s")
     print(f"Sleeping for {remaining_sleep:.2f}s to keep traders in sync\n")
